4d61f3ba3cbc36f92444cf5bab97896e91b9e65e
Zhu, Nan <nz18977@imcap.ap.ssmb.com>
2019-02-21 15:44:49 +0800

    seperate Controller with RestController

--- src/main/java/com/nan/hellokitty/controller/IndexController.java
+++ src/main/java/com/nan/hellokitty/controller/IndexController.java
@@ -1,12 +1,12 @@
 package com.nan.hellokitty.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class IndexController {
 	@RequestMapping("/")
 	public String index() {
-		return "HelloWorld";
+		return "hellokitty.html";
 	}
 }
